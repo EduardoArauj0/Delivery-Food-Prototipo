85ef7c9c3f0c588ed2ea8599ffafa1703ba61c7c
Eduardo Araujo <eduardoaraujo.contato13@gmail.com>
2025-04-21 19:22:59 -0300

    feat: implementa carrinho funcional com botão flutuante, visibilidade dinâmica e remoção de itens

---
+++ js/carrinho.js
@@ -0,0 +1,33 @@
+  const carrinho = [];
+  
+  function adicionarAoCarrinho(produto) {
+    carrinho.push(produto);
+    atualizarCarrinho();
+  
+    const aside = document.getElementById("carrinho");
+    aside.classList.add("visivel");
+  }  
+
+  function alternarCarrinho() {
+    const aside = document.getElementById("carrinho");
+    aside.classList.toggle("visivel");
+  }  
+
+  function atualizarCarrinho() {
+    const lista = document.getElementById("itens-carrinho");
+    lista.innerHTML = "";
+  
+    carrinho.forEach((item, index) => {
+      const li = document.createElement("li");
+      li.innerHTML = `
+        ${item} 
+        <span class="remover" onclick="removerDoCarrinho(${index})">❌</span>
+      `;
+      lista.appendChild(li);
+    });
+  }
+  
+  function removerDoCarrinho(index) {
+  carrinho.splice(index, 1);
+  atualizarCarrinho();
+}
