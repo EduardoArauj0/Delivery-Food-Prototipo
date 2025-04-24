2b402dac582fe51c197a2c2a59d2420861dfc917
Eduardo Araujo <eduardoaraujo.contato13@gmail.com>
2025-04-24 18:57:16 -0300

    refactor: reestrutura HTML do painel do restaurante para suportar JS modularizado

--- js/painel-restaurante.js
+++
@@ -1,5 +0,0 @@
-function alternarAba(abaId) {
-    document.querySelectorAll(".aba").forEach((aba) => aba.classList.remove("ativa"));
-    document.getElementById("aba-" + abaId).classList.add("ativa");
-  }
-  
